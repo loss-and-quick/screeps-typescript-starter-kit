7e723fc500dd622837f7d33ac95ba87f277aba74
minicx <minicx@disroot.org>
2023-03-24 01:22:04 +0300

    first init!

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+
+import clear from 'rollup-plugin-clear';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import screeps from 'rollup-plugin-screeps';
+
+import config from './screeps.json' assert { type: 'json' };
+
+if (process.env.DEST && config[process.env.DEST] !== null) {
+    if (config[process.env.DEST] === null) {
+        throw new Error("Wrong destination passed or screeps.json is not found!");
+    } 
+}
+export default {
+    input: "src/main.ts",
+    output: {
+        file: "dist/main.js",
+        format: "cjs",
+        sourcemap: true
+    },
+    plugins: [
+        clear({ targets: ["dist"] }),
+        resolve({ rootDir: "src" ,preferBuiltins: true,browser:true,preferBuiltins:true}),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" ,sourceMap:true}),
+        process.env.DEST ? screeps({ config: {...config,branch:process.env.DEST.toLowerCase()}, }) : undefined
+    ]
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+
+export const loop = (): void => {
+    console.log('hello world!');
+};

1051dbbf7eff0ca2d311bef720977a208dbd99d5
minicx <minicx@disroot.org>
2023-03-24 01:52:30 +0300

    Migration to `rollup-plugin-typescript2` Reason: It has built-in compiler errors

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import clear from 'rollup-plugin-clear';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import screeps from 'rollup-plugin-screeps';
 
 import config from './screeps.json' assert { type: 'json' };
@@ -21,9 +21,9 @@ export default {
     },
     plugins: [
         clear({ targets: ["dist"] }),
-        resolve({ rootDir: "src" ,preferBuiltins: true,browser:true,preferBuiltins:true}),
+        resolve({ rootDir: "src", preferBuiltins: true, browser:true, preferBuiltins:true}),
         commonjs(),
-        typescript({ tsconfig: "./tsconfig.json" ,sourceMap:true}),
+        typescript({ tsconfig: "./tsconfig.json"}),
         process.env.DEST ? screeps({ config: {...config,branch:process.env.DEST.toLowerCase()}, }) : undefined
     ]
 }
